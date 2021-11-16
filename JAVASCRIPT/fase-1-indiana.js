22cb599bab49f74960c28dc40c6d9a8e5ecaa3b2
LagAntunes <lag151617@gmail.com>
2021-11-16 14:59:05 -0300

    primeiro commit

---
+++ JAVASCRIPT/fase-1-indiana.js
@@ -0,0 +1,16 @@
+// início da programação da primeira fase do indiana //
+function escolha(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../personagens.html');
+    } else if (destino == 'B') {
+        return location = ('../index.html');
+    } else {
+        return escolha()
+    }
+}
+// fazer uma página jvs pra cada fase***************************
+
+
+// fim da programação da primeira fase do indiana //

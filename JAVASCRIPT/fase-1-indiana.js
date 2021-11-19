40c0089de5833da4665a9e381f82892f0b1420e6
LagAntunes <lag151617@gmail.com>
2021-11-18 21:18:28 -0300

    atualização de projeto

--- JAVASCRIPT/fase-1-indiana.js
+++ JAVASCRIPT/fase-1-indiana.js
@@ -1,16 +1,41 @@
-// início da programação da primeira fase do indiana //
-function escolha(){
+// início da programação da primeira fase do indiana (carta) //
+function escolha1(){
     alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
     var destino = prompt("Escolha seu destino");
     if (destino == 'A') {
-        return location = ('../personagens.html');
+        return location = ('../game-over-indiana-fase1.html');
     } else if (destino == 'B') {
-        return location = ('../index.html');
+        return location = ('../fase-1-1-indiana.html');
     } else {
-        return escolha()
+        return escolha1()
     }
 }
-// fazer uma página jvs pra cada fase***************************
+// fim da programação da primeira fase do indiana (carta) //
 
+// início da programação da primeira fase do indiana (floresta1) //
+function escolha2(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../fase-1-3-indiana.html');
+    } else if (destino == 'B') {
+        return location = ('../game-over-indiana-fase1-2.html');
+    } else {
+        return escolha2()
+    }
+}
+// fim da programação da primeira fase do indiana (floresta1) //
 
-// fim da programação da primeira fase do indiana //
+// início da programação da primeira fase do indiana (floresta2) //
+function escolha3(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../game-over-indiana-fase1-3.html');
+    } else if (destino == 'B') {
+        return location = ('../fase-1-4-indiana.html');
+    } else {
+        return escolha2()
+    }
+}
+// fim da programação da primeira fase do indiana (floresta2) //

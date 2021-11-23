11e53dc2d002b476dd4c6e933799c34f81ff9fec
LagAntunes <lag151617@gmail.com>
2021-11-22 23:27:08 -0300

    atualização de projeto

---
+++ JAVASCRIPT/fase-indiana.js
@@ -0,0 +1,83 @@
+// início da programação da primeira fase do indiana (carta) //
+function escolha1(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../game-over-indiana-fase1-1.html');
+    } else if (destino == 'B') {
+        return location = ('../fase-1-1-indiana.html');
+    } else {
+        return escolha1()
+    }
+}
+// fim da programação da primeira fase do indiana (carta) //
+
+// início da programação da primeira fase do indiana (floresta1) //
+function escolha2(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../fase-1-3-indiana.html');
+    } else if (destino == 'B') {
+        return location = ('../game-over-indiana-fase1-2.html');
+    } else {
+        return escolha2()
+    }
+}
+// fim da programação da primeira fase do indiana (floresta1) //
+
+// início da programação da primeira fase do indiana (floresta2) //
+function escolha3(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../game-over-indiana-fase1-3.html');
+    } else if (destino == 'B') {
+        return location = ('../fase-1-4-indiana.html');
+    } else {
+        return escolha3()
+    }
+}
+// fim da programação da primeira fase do indiana (floresta2) //
+
+// início da programação da segunda fase do indiana (deserto1) //
+function escolha4(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../game-over-indiana-fase2-0.html');
+    } else if (destino == 'B') {
+        return location = ('../fase-2-1-indiana.html');
+    } else {
+        return escolha4()
+    }
+}
+// fim da programação da segunda fase do indiana (deserto1) //
+
+// início da programação da última fase do indiana (mar1) //
+function escolha5(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../game-over-indiana-fase3-0.html');
+    } else if (destino == 'B') {
+        return location = ('../fase-3-1-indiana.html');
+    } else {
+        return escolha5()
+    }
+}
+// fim da programação da última fase do indiana (mar1) //
+
+// início da programação da última fase do indiana (atlântida1) //
+function escolha6(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../fase-3-5-indiana.html');
+    } else if (destino == 'B') {
+        return location = ('../game-over-indiana-fase3-1.html');
+    } else {
+        return escolha6()
+    }
+}
+// fim da programação da última fase do indiana (atlântida1) //
---
+++ JAVASCRIPT/fase-lara.js
@@ -0,0 +1,13 @@
+// início da programação da primeira fase da lara (fuga) //
+function escolha1(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../');
+    } else if (destino == 'B') {
+        return location = ('../');
+    } else {
+        return escolha1()
+    }
+}
+// fim da programação da primeira fase da lara (fuga) //

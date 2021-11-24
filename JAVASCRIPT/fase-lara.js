96e58a2965e384f7433ed1469b09bb34f9e3ed8c
LagAntunes <lag151617@gmail.com>
2021-11-24 10:20:39 -0300

    atualização de projeto

--- JAVASCRIPT/fase-lara.js
+++ JAVASCRIPT/fase-lara.js
@@ -25,3 +25,17 @@ function escolha2(){
     }
 }
 // fim da programação da segunda fase da lara (Egito) //
+
+// início da programação da última fase da lara (Namíbia) //
+function escolha3(){
+    alert ("Escolha e digite no prompt alguma das alternativas A ou B (em maiúsculo e somente uma opção)");
+    var destino = prompt("Escolha seu destino");
+    if (destino == 'A') {
+        return location = ('../lfase-3-3-lara.html');
+    } else if (destino == 'B') {
+        return location = ('../lgame-over-lara-fase-3-0.html');
+    } else {
+        return escolha3()
+    }
+}
+// fim da programação da última fase da lara (Namíbia) //
